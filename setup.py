a95eb821818e92a4128f49e8a83938b40d1c6bac
M Gandhi <148590629+mustafagandhi@users.noreply.github.com>
2023-10-23 15:14:11 -0400

    V0.1

--- setup.py
+++ setup.py
@@ -11,3 +11,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="projekt-pdf-lib",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Add your library's dependencies here. For example:
+        'pytest==7.4.2',
+    ],
+    author="Mustafa Gandhi",
+    author_email="148590629+mustafagandhi@users.noreply.github.com",
+    description="A Python based PDF manipulation library.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mustafagandhi/projekt-pdf-lib",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0 with Commons Clause Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
